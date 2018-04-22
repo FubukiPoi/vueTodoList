fa44b1f53f755b3ce305f8605399e8aebf5c95b0
FatDonkey <fatdonkeypoi@gmail.com>
2018-04-22 16:45:22 +0800

    Initial commit

---
+++ src/TodoList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="todolist">
+    <input v-model="inputValue"/>
+    <button @click="handleSubmit">submit</button>
+    <ul>
+      <todo-item 
+      v-for="(item,index) of list" 
+      :key="index"
+      :content="item" 
+      :index="index" 
+      @delete="handleDelete">
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem";
+
+export default {
+  components: {
+    "todo-item": TodoItem
+  },
+  data() {
+    return {
+      inputValue: "",
+      list: []
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.list.push(this.inputValue);
+      this.inputValue = "";
+    },
+    handleDelete(){
+      this.list.splice(this.index,1);
+    }
+  }
+};
+</script>
+
+<style>
+#todolist {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <li>{{content}}<button @click="handleDelete()">delete</button></li>
+    
+</div>
+</template>
+
+<script>
+export default {
+  props: ["content", "index"],
+  methods: {
+    handleDelete() {
+      //this.$emit()与父组件通信
+      this.$emit("delete", this.index);
+    }
+  }
+};
+</script>
+
+<style>
+li {
+  list-style-type: none;
+}
+</style>
